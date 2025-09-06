6a1dc05964a82b1f7665ca80f878d39e2b69b750
prmalatjhb024 <prudentmalatjie@gmail.com>
2025-09-06 13:08:56 +0200

    added src files code

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-800 text-white flex flex-col items-center justify-center font-sans">
+      <div className="text-center p-8">
+        <h1 className="text-5xl font-bold text-cyan-400">Crypto Quest Study Buddy</h1>
+        <p className="mt-4 text-lg text-gray-300">Your web3 learning adventure is about to begin!</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
